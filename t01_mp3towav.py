ed3eb582e14aff29b4fe28eaa6d1821a5184a73d
Guifonte <guilhermenishifonte@gmail.com>
2018-09-04 11:25:20 -0300

    comments added

--- t01_mp3towav.py
+++ t01_mp3towav.py
@@ -6,18 +6,21 @@ import librosa
 import os
 import pydub
 
-
+#name of the two instruments
 instr1_name = "flute"
 instr2_name = "viola"
 
+#folder initialization
 instr1_main_folder = './' + instr1_name + "/"
 instr2_main_folder = './' + instr2_name + "/"
 instr1_wav_folder = './' + instr1_name + "_wav/"
 instr2_wav_folder = './' + instr2_name + "_wav/"
 
+#get the lists of mp3 files
 instr1_files = os.listdir(instr1_main_folder)
 instr2_files = os.listdir(instr2_main_folder)
 
+#filter the lists of files
 instr1 = [k for k in instr1_files if 'very-long' in k]
 instr1 += [k for k in instr1_files if '_long_' in k]
 instr2 = [k for k in instr2_files if 'pizz-normal' in k]
@@ -27,6 +30,7 @@ instr2_len = len(instr2)
 instr1_lst = [None] * instr1_len
 instr2_lst = [None] * instr2_len
 
+#clear the old wav files if exists
 instr1_wav_list = os.listdir(instr1_wav_folder)
 instr2_wav_list = os.listdir(instr2_wav_folder)
 
@@ -39,6 +43,7 @@ for i in range(0,len(instr2_wav_list)):
 print("Samples of " + instr1_name + ":", instr1_len)
 print("Samples of " + instr2_name + ":", instr2_len)
 
+#convert from mp3 to wav all the filtered files from instrument 1 and instrument 2 and save
 for i in range(0,instr1_len):
     sound = pydub.AudioSegment.from_mp3(instr1_main_folder + instr1[i])
     sound.export(instr1_wav_folder + instr1[i][:-4] + ".wav",format ="wav")
--- t01_pvalue.py
+++ t01_pvalue.py
@@ -5,35 +5,41 @@ import numpy as np
 import librosa
 import os
 
-flute = os.listdir('./flute_wav/')
-viola = os.listdir('./viola_wav/')
-flute_len = len(flute)
-viola_len = len(viola)
-flute_lst = [None] * flute_len
-flute_stft = [None] * flute_len
-flute_mean = [None] * flute_len
-flute_stdv = [None] * flute_len
-flute_flat = [None] * flute_len
-viola_lst = [None] * viola_len
-viola_stft = [None] * viola_len
-viola_mean = [None] * viola_len
-viola_stdv = [None] * viola_len
-viola_flat = [None] * viola_len
+instr1_name = "flute"
+instr2_name = "viola"
 
-print("Samples of flute:", flute_len)
-print("Samples of viola", viola_len)
+instr1_wav_folder = './' + instr1_name + "_wav/"
+instr2_wav_folder = './' + instr2_name + "_wav/"
 
-for i in range(0,flute_len):
-    flute_lst[i], fs = librosa.load('./flute_wav/' + flute[i], sr=44100)
-    flute_stft[i] = librosa.stft(flute_lst[i], n_fft=2048, hop_length=128, win_length=1024, window='hann')
-    flute_flat[i] = librosa.feature.spectral_flatness(S=np.abs(flute_stft[i]), n_fft=2048,hop_length=512)
-    flute_mean[i], flute_stdv[i] = np.mean(flute_flat[i]), np.std(flute_flat[i])
+instr1 = os.listdir(instr1_wav_folder)
+instr2 = os.listdir(instr2_wav_folder)
+instr1_len = len(instr1)
+instr2_len = len(instr2)
+instr1_lst = [None] * instr1_len
+instr1_stft = [None] * instr1_len
+instr1_mean = [None] * instr1_len
+instr1_stdv = [None] * instr1_len
+instr1_flat = [None] * instr1_len
+instr2_lst = [None] * instr2_len
+instr2_stft = [None] * instr2_len
+instr2_mean = [None] * instr2_len
+instr2_stdv = [None] * instr2_len
+instr2_flat = [None] * instr2_len
 
-for i in range(0,viola_len):
-    viola_lst[i], fs = librosa.load('./viola_wav/' + viola[i], sr=44100)
-    viola_stft[i] = librosa.stft(viola_lst[i], n_fft=2048, hop_length=128, win_length=1024, window='hann')
-    viola_flat[i] = librosa.feature.spectral_flatness(S=np.abs(viola_stft[i]), n_fft=2048,hop_length=512)
-    viola_mean[i], viola_stdv[i] = np.mean(viola_flat[i]), np.std(viola_flat[i])
+print("Samples of " + instr1_name + ":", instr1_len)
+print("Samples of " + instr2_name + ":", instr2_len)
+
+for i in range(0,instr1_len):
+    instr1_lst[i], fs = librosa.load(instr1_wav_folder + instr1[i], sr=44100)
+    instr1_stft[i] = librosa.stft(instr1_lst[i], n_fft=2048, hop_length=128, win_length=1024, window='hann')
+    instr1_flat[i] = librosa.feature.spectral_flatness(S=np.abs(instr1_stft[i]), n_fft=2048,hop_length=512)
+    instr1_mean[i], instr1_stdv[i] = np.mean(instr1_flat[i]), np.std(instr1_flat[i])
+
+for i in range(0,instr2_len):
+    instr2_lst[i], fs = librosa.load(instr2_wav_folder + instr2[i], sr=44100)
+    instr2_stft[i] = librosa.stft(instr2_lst[i], n_fft=2048, hop_length=128, win_length=1024, window='hann')
+    instr2_flat[i] = librosa.feature.spectral_flatness(S=np.abs(instr2_stft[i]), n_fft=2048,hop_length=512)
+    instr2_mean[i], instr2_stdv[i] = np.mean(instr2_flat[i]), np.std(instr2_flat[i])
 
 #d0 = librosa.stft(x1, n_fft=2048, hop_length=128, win_length=1024, window='hann')
 #centX = librosa.feature.spectral_centroid(S=np.abs(d0), n_fft=2048,\
@@ -56,10 +62,10 @@ for i in range(0,viola_len):
 #mcx3, scx3 = np.mean(centX), np.std(centX)
 
 
-means_viola = np.mean ( np.array(viola_mean))
-stds_viola =  np.std ( np.array(viola_stdv))
-means_flute = np.mean ( np.array(flute_mean))
-stds_flute =  np.std ( np.array(flute_stdv))
+means_instr2 = np.mean ( np.array(instr2_mean))
+stds_instr2 =  np.std ( np.array(instr2_stdv))
+means_instr1 = np.mean ( np.array(instr1_mean))
+stds_instr1 =  np.std ( np.array(instr1_stdv))
 
 #means_crash = np.mean ( np.array( [scx0, scx1] ))
 #stds_crash =  np.std ( np.array( [scx0, scx1] ))
@@ -67,8 +73,8 @@ stds_flute =  np.std ( np.array(flute_stdv))
 #stds_horn =  np.std ( np.array( [scx2, scx3] ))
 
 
-t, p = st.ttest_ind_from_stats(means_viola, stds_viola, viola_len,\
-                               means_flute, stds_flute, flute_len)
-print(means_viola, means_flute)
-print(stds_viola, stds_flute)
+t, p = st.ttest_ind_from_stats(means_instr2, stds_instr2, instr2_len,\
+                               means_instr1, stds_instr1, instr1_len)
+print(means_instr2, means_instr1)
+print(stds_instr2, stds_instr1)
 print("P-value", p)
